a3470d1d7a1c12404fcd0b5875cdb4ff22922551
Tommy Chabiras <tommychabiras@gmail.com>
2026-04-10 02:22:36 -0400

    added data loading and created evaluation.py file

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,7 @@
 import cv2
-
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def preprocess_image(img):
     img = img[60:135, :, :]
@@ -15,3 +17,25 @@ def load_and_preprocess(image_path):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = preprocess_image(img)
     return img
+
+
+def load_data():
+    df = pd.read_csv("../dataset/driving_log.csv", header=None)
+
+    data_list = []
+    label_list = []
+
+    for _, row in df.iterrows():
+        center = row[0].strip()
+        angle = float(row[3])
+
+        img = load_and_preprocess(center)
+        data_list.append(img)
+        label_list.append(angle)
+
+    x = np.array(data_list)
+    y = np.array(label_list)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+    return x_train, x_test, y_train, y_test
